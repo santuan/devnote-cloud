9f637c78310ec0e4a079225d2e2d3052763e0333
santuan <smrcouto88@gmail.com>
2025-01-31 02:11:10 -0300

    bump up, remove autoanimate

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue"
 import { createPinia } from "pinia"
-import { autoAnimatePlugin } from "@formkit/auto-animate/vue"
 import "./assets/main.css"
 import "./assets/driver.css"
 import App from "./App.vue"
@@ -11,7 +10,6 @@ const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 const app = createApp(App)
 app.use(pinia)
-app.use(autoAnimatePlugin)
 app.use(i18n)
 
 app.mount("#app")
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
+
 import vue from "@vitejs/plugin-vue"
 import tailwind from "tailwindcss"
 import autoprefixer from "autoprefixer"
@@ -36,7 +37,24 @@ export default defineConfig({
   // server: {
   //   port: 4173,
   // },
+  
   build: {
+    // rollupOptions: {
+    //   output: {
+    //     manualChunks(id: string) {
+    //       if (
+    //         id.includes('shiki/bundle/web') ||
+    //         id.includes('vue-i18n') ||
+    //         id.includes('reka-ui') ||
+    //         id.includes('@tiptap/vue-3') ||
+    //         id.includes('pinia') ||
+    //         id.includes('@vueuse/core') 
+    //       ) {
+    //         return '@tiptap/vue-3';
+    //       }
+    //     },
+    //   },
+    // },
     chunkSizeWarningLimit: 2000,
     // rollupOptions: {
     //   external: ['workbox-window']
